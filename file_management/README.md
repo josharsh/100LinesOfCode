9dcc7c51b71a3b6f21f005676cadf2bf0fdeca6d
imbush <inlebush@gmail.com>
2021-07-10 07:43:49 -0400

    imbush | files.py, README.md | Finish README, add docstrings to functions

--- file_management/README.md
+++ file_management/README.md
@@ -1,19 +1,22 @@
 # File Management Functions
 Useful mass file management functions in Python3. 
 
-These three functions also make good templates for new functions that meet more specific needs.
+These three functions can be used as templates for new functions that meet more specific needs.
 
 ## Functions
-1. ```replace(to_repl: str, repl: str, path="", walk=False)```:
-Replaces to_repl string with repl string in the file names 
-
-2. add_prefix_suffix(fix: str, add_as="prefix", extension="", path="", walk=False):
+1. ```replace(to_repl: str, repl: str, path="", walk=False)```:  
+    Replaces to_repl string with repl string in file names within folder.
 
 
-3. collect(key: str, folder_name: str, key_location="contains", extension="", path="")
+2. ```add_prefix_suffix(fix: str, add_as="prefix", extension="", path="", walk=False)```:  
+Add fix string as prefix or suffix before filetype to all files or files with extension if specified.
 
+3. ```collect(key: str, folder_name: str, key_location="contains", extension="", path="")```:  
+Move files with key located in filename to folder names folder_name.
+Creates folder with folder_name if folder doesn't exist.
 
 ## Usage
+Download python if not downloaded. Create python file, import functions and call functions.
 ``` python
 from files.py import replace, add_prefix_suffix, collect
 
--- file_management/files.py
+++ file_management/files.py
@@ -2,10 +2,14 @@ import os
 import shutil
 
 
-def replace(to_repl: str, repl: str, path="", walk=False):
+def replace(to_repl: str, repl: str, path="", walk=False) -> None:
     """Replace to_repl string with repl string in all file names.
 
-    Ignores hidden files and folders.
+    Parameters:
+        to_repl (str): String being replaced
+        repl (str): String to replace with
+        path (str): File directory. By default, current directory
+        walk (bool): If True, renames files in subfolders also
     """
     if not path:
         path = os.getcwd()
@@ -16,21 +20,25 @@ def replace(to_repl: str, repl: str, path="", walk=False):
             for filename in files:
                 if filename[0] != ".":
                     file, file_extension = os.path.splitext(filename)
-                    os.rename(
-                                os.path.join(root, filename),
-                                os.path.join(root, file.replace(to_repl, repl) + file_extension))
+                    os.rename(os.path.join(root, filename),
+                              os.path.join(root, file.replace(to_repl, repl) + file_extension))
     else:
         for filename in next(os.walk(path))[2]:
             if filename[0] != ".":
                 file, file_extension = os.path.splitext(filename)
-                os.rename(
-                            os.path.join(path, filename),
-                            os.path.join(path, file.replace(to_repl, repl) + file_extension))
+                os.rename(os.path.join(path, filename),
+                          os.path.join(path, file.replace(to_repl, repl) + file_extension))
 
 
-def add_prefix_suffix(fix: str, add_as="prefix", extension="", path="", walk=False):
-    """Add string as prefix or suffix before filetype to
-    all files or files with extension if specified.
+def add_prefix_suffix(fix: str, add_as="prefix", extension="", path="", walk=False) -> None:
+    """Add fix string as prefix or suffix before filetype to all files or files with extension if specified.
+    
+    Parameters:
+        fix (str): String to add as prefix or suffix
+        add_as (str): 'prefix' or 'suffix'
+        extension (str): Optionally specify file extension of files that can be renamed
+        path (str): File directory. By default, current directory
+        walk (bool): If True, renames files in subfolders also
     """
     if not path:
         path = os.getcwd()
@@ -42,34 +50,34 @@ def add_prefix_suffix(fix: str, add_as="prefix", extension="", path="", walk=Fal
                 file, file_extension = os.path.splitext(filename)
                 if filename[0] != "." and (file_extension == extension or not extension):
                     if add_as == "prefix":
-                        os.rename(
-                                    os.path.join(root, filename),
-                                    os.path.join(root, fix + filename))
+                        os.rename(os.path.join(root, filename),
+                                  os.path.join(root, fix + filename))
                     elif add_as == "suffix":
-                        os.rename(
-                                    os.path.join(root, filename),
-                                    os.path.join(root, file + fix + file_extension))
+                        os.rename(os.path.join(root, filename),
+                                  os.path.join(root, file + fix + file_extension))
 
     else:
         for filename in next(os.walk(path))[2]:
             file, file_extension = os.path.splitext(filename)
             if filename[0] != "." and (file_extension == extension or not extension):
                 if add_as == "prefix":
-                    os.rename(
-                                os.path.join(path, filename),
-                                os.path.join(path, fix + filename))
+                    os.rename(os.path.join(path, filename),
+                              os.path.join(path, fix + filename))
                 elif add_as == "suffix":
-                    os.rename(
-                                os.path.join(path, filename),
-                                os.path.join(path, file + fix + file_extension))
+                    os.rename(os.path.join(path, filename),
+                              os.path.join(path, file + fix + file_extension))
 
 
-def collect(key: str, folder_name: str, key_location="contains", extension="", path=""):
-    """
-    Moves files with key located in filename to folder names folder_name.
-    Create folder with folder_name if folder doesn't exist.
+def collect(key: str, folder_name: str, key_location="contains", extension="", path="") -> None:
+    """Move files with key located in filename to folder names folder_name.
+    Creates folder with folder_name if folder doesn't exist.
 
-    key_location: contains, prefix, suffix
+    Parameters:
+        key (str): String to check for
+        folder_name (str): String specifing folder name to move files into.
+        key_location (str): 'contains', 'prefix' or 'suffix'. Specifies location of string. 
+        extension (str): Optionally specify file extension of files that can be moved
+        path (str): File directory. By default, current directory
     """
     if not path:
         path = os.getcwd()
@@ -78,7 +86,8 @@ def collect(key: str, folder_name: str, key_location="contains", extension="", p
 
     # Add folder if folder doesn't exist
     destination = os.path.join(path, folder_name)
-    os.mkdir(destination)
+    if not os.path.isdir(destination):
+        os.mkdir(destination)
 
     for filename in next(os.walk(path))[2]:
         if filename[0] != ".":
@@ -87,7 +96,5 @@ def collect(key: str, folder_name: str, key_location="contains", extension="", p
                 if (key_location == "contains" and key in file or
                         key_location == "prefix" and file[:len(key)] == key or
                         key_location == "suffix" and file[-len(key):]):
-                    shutil.move(
-                                os.path.join(path, file),
-                                os.path.join(destination, file))
-
+                    shutil.move(os.path.join(path, filename),
+                                os.path.join(destination, filename))
