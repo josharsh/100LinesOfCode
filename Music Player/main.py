a19acc599b510bc140e411d53537f5bcfdea56d3
saicharan21-dev <75615707+saicharan21-dev@users.noreply.github.com>
2021-08-26 00:24:18 +0530

    Update main.py

--- Music Player/main.py
+++ Music Player/main.py
@@ -34,28 +34,28 @@ songstatus.set("choosing")
 
 #playlist---------------
 
-playlist=Listbox(root,selectmode=SINGLE,bg="DodgerBlue2",fg="white",font=('arial',15),width=40)
+playlist=Listbox(root,selectmode=SINGLE,bg="aqua",fg="black",font=('arial',25),width=35)
 playlist.grid(columnspan=5)
 
-os.chdir(r'C:/Project/Songs')
+os.chdir(r'C:\Users\saich\OneDrive\Desktop\songs') #add location of the files when you are using it in local computer
 songs=os.listdir()
 for s in songs:
     playlist.insert(END,s)
 
 playbtn=Button(root,text="play",command=playsong)
-playbtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+playbtn.config(font=('arial',25),bg="white",fg="black",padx=7,pady=7)
 playbtn.grid(row=1,column=0)
 
 pausebtn=Button(root,text="Pause",command=pausesong)
-pausebtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+pausebtn.config(font=('arial',25),bg="white",fg="black",padx=7,pady=7)
 pausebtn.grid(row=1,column=1)
 
 stopbtn=Button(root,text="Stop",command=stopsong)
-stopbtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+stopbtn.config(font=('arial',25),bg="white",fg="black",padx=7,pady=7)
 stopbtn.grid(row=1,column=2)
 
 Resumebtn=Button(root,text="Resume",command=resumesong)
-Resumebtn.config(font=('arial',20),bg="DodgerBlue2",fg="white",padx=7,pady=7)
+Resumebtn.config(font=('arial',25),bg="white",fg="black",padx=7,pady=7)
 Resumebtn.grid(row=1,column=3)
 
 mainloop()
