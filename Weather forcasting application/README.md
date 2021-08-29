74500a8b3cecf5593011c805cc2d971df9ad0736
dhruv-tripathi <dhruvtripathi2407@gmail.com>
2021-08-30 01:17:03 +0530

    Dhruv|Weather forcasting application

---
+++ Weather forcasting application/README.md
@@ -0,0 +1,8 @@
+# About
+I have built a python application for visualizing weather data with the usage of OpenWeatherMap APIs. This project also encompasses the usage of
+Streamlit, a low code front end for Data Scientists.
+# Usage
+To run the app, just install the Streamlit, PyOWM, Matplotlib and DateTime libraries using "pip install "library name" ".
+to run the application we need to type " streamlit run <filename> "
+#Author 
+Dhruv Tripathi
---
+++ Weather forcasting application/app.py
@@ -0,0 +1,23 @@
+import os
+import pyowm
+import streamlit as st
+from matplotlib import dates
+from datetime import datetime
+from matplotlib import pyplot as plt
+owm = pyowm.OWM('55635f0edc2fbe3e3ada8d7bf0f8c0e9')
+mgr = owm.weather_manager()
+st.title("5 Day Weather Forecast")
+st.write("### Write the name of a City and select the Temperature Unit and Graph Type from the sidebar")
+place = st.text_input("NAME OF THE CITY :", "")
+if place is None:
+    st.write("Input a CITY!")
+unit = st.selectbox("Select Temperature Unit", ("Celsius", "Fahrenheit"))
+g_type = st.selectbox("Select Graph Type", ("Line Graph", "Bar Graph"))
+observation = mgr.forecast_at_place(place, '3h')
+plt.bar(days, temp_min)
+plt.bar(days, temp_max)
+forecaster = mgr.forecast_at_place(place, '3h')
+print(forecaster.will_have_rain())
+print(weather.sunrise_time(timeformat='iso'))
+humidity = weather.humidity
+print(f'The current humidity is {humidity}%')
