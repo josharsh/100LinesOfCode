51fce89b7af79a50716aaa941e38bba783a019b9
om-umrania-code <60146744+om-umrania@users.noreply.github.com>
2022-10-01 16:33:31 +0530

    Improving 100lines.cpp
    Improving the code readability of 100lines Generator/100lines.cpp code
    Completing Issue #244

--- 100lines Generator/100lines.cpp
+++ 100lines Generator/100lines.cpp
@@ -3,22 +3,26 @@
 #include <time.h>
 #include <iomanip>
 
+using namespace std;
+
 int main() {
-    std::srand(time(nullptr));
+    srand(time(nullptr));
     int instructions;
     int bits;
-    std::cout << "How many bits each instruction should have?" << std::endl;
-    std::cin >> bits;
-    std::cout << "How many instruction you want to generate per line?" << std::endl;
-    std::cin >> instructions;
+    cout << "How many bits each instruction should have?" << std::endl;
+    cin >> bits;
+    cout << "How many instruction you want to generate per line?" << std::endl;
+    cin >> instructions;
     for (int i = 0; i < 100; ++i) {
         for (int k = 0; k < instructions; ++k) {
             for (int j = 0; j < bits; ++j) {
-                std::cout << rand() % 2;
+                cout << rand() % 2;
             }
-            std::cout << " ";
+            cout << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
-    std::cout << "I hope it doesn't have any bugs!";
+    cout << "I hope it doesn't have any bugs!";
+
+    return 0;
 }
