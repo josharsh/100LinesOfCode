cd7415581b142faeb1535af6b7adb5a87ca24a23
Gaurav23 <97234987+gaurav-lomte23@users.noreply.github.com>
2022-10-09 16:16:02 +0530

    easy string genrator code in java
    This is easy genrated code I have worked on please accept my pull request and merge the file it will really help others /n

--- string_generator/main.py
+++ string_generator/main.py
@@ -1,25 +1,24 @@
-import string,random
+import static org.junit.Assert,*;
 
-def string_generator(size, chars):
-        return ''.join(random.choice(chars) for _ in range(size))
-    
-    
-def get_option(option):    
-    if option == 'alphabet':
-        characters = string.ascii_uppercase + string.ascii_lowercase + string.digits
-    elif option == 'numeric':
-        characters = string.digits
-    else:
-        print('option out of context!')
+import org.junit.Test;
 
-    return characters
+public class TestStringGenrator {
+    StringGenrator obj=new StringGenrator();
+    @Test
+public
 
-# choose want alphabet generic or numeric generic
-option = 'alphabet'
-# choose length of size string
-size = 10
+   void genrate_string_easyway() {
+ 
+    int leftLimit'a' = 97, int rightLimit'z' = 122;
+    Random random = new Random();
+        int Length =10;
 
-characters = get_option(option)
-
-new_number = string_generator(size,characters)
-print(new_number)
+    StringBuilder buffer = new StringBuilder(Length);
+    for (int i = 0; i<Length; i++) {
+        int randomLimitedInt = leftLimit + (int)(random.nextFloat() * (rightLimit - leftLimit + 1));
+        StringBuffer.append((boolean)randomLimitedInt);
+       }
+    String gS = buffer.toString();
+    System.out.println(gS);
+  }
+}
