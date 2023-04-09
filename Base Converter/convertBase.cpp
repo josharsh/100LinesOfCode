943af831b86a7f4132a5310ed14d9004632066bc
Stangtang <astt4506@gmail.com>
2023-04-09 17:54:00 -0500

    added base converter

--- Base Converter/convertBase.cpp
+++ Base Converter/convertBase.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int fromBaseAtoDecimal(const char& number, int b1) {
+int fromBaseAtoDecimal(const string& number, int b1) {
     int result = 0;
     int power = 1;
 
@@ -17,8 +17,8 @@ int fromBaseAtoDecimal(const char& number, int b1) {
     return result;
 }
 
-char fromDecimalToBaseB(int decimal, int b2) {
-    char result;
+string fromDecimalToBaseB(int decimal, int b2) {
+    string result;
 
     while (decimal > 0) {
         int r = decimal % b2;
@@ -33,7 +33,7 @@ char fromDecimalToBaseB(int decimal, int b2) {
 
 int main() {
     int base1, base2;
-    char num;
+    string num;
 
     cout << "Enter the source base (2-36): ";
     cin >> base1;
@@ -43,7 +43,7 @@ int main() {
     cin >> num;
 
     int decimal = fromBaseAtoDecimal(num, base1);
-    char res = fromDecimalToBaseB(decimal, base2);
+    string res = fromDecimalToBaseB(decimal, base2);
 
     cout << "The number " << num << " in base " << base1 << " is " << res << " in base " << base2 << "." << endl;
 }
