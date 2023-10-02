fbfe995287e3baf7f69ffc8466297f14793763f7
Ahmad Habibzad <ahabibzad@meritamerica.org>
2023-10-02 17:57:06 -0400

    change variable names to follow Java naming convention

--- Password-Generator/Password.java
+++ Password-Generator/Password.java
@@ -4,17 +4,17 @@ public class Password{
         int number[] = {0,1,2,3,4,5,6,7,8,9};
         int randomAlpha = 0;
         int randomNum = 0;
-        String PasswordString = "";
-        String PasswordNum = "";
+        String passwordString = "";
+        String passwordNum = "";
 
         do{
             randomAlpha=(int)(Math.random()*alpha.length);
-            PasswordString+=alpha[randomAlpha];
+            passwordString+=alpha[randomAlpha];
             randomNum=(int)(Math.random()*number.length);
-            PasswordNum+=String.valueOf(number[randomNum]);
-        }while(PasswordString.length()!=4);
+            passwordNum+=String.valueOf(number[randomNum]);
+        }while(passwordString.length()!=4);
 
-        String finalPassword = PasswordString.substring(0,1).toUpperCase()+PasswordString.substring(1)+"@"+PasswordNum;
+        String finalPassword = passwordString.substring(0,1).toUpperCase()+passwordString.substring(1)+"@"+passwordNum;
          System.out.println("Your password is "+finalPassword);
     }
 }
