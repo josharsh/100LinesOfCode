76c8674f4ef34559e261abbd0b97fd6b31454654
nimrit-singh <nimritsingh377@gmail.com>
2023-10-28 17:27:36 +0530

    nimrit-singh|collatz.py| integrated streamlit

--- collatz.py
+++ collatz.py
@@ -1,17 +1,19 @@
 """ This is a project on a famous mathematical collatz conjecture"""
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit as st
+import pandas as pd
 def collatz(num):
     result=num
+   # int(result)
     Lx=[]
     Ly=[]
     count=0
-    while result!=1: #if num is even divide by 2
-    #if num is odd 3n+1
-        if result%2==0:
+    while result!=1:
+        if result%2==0: #if num is even divide by 2
            result=result//2
            Lx.append(result)
-        elif result%2==1:
+        elif result%2==1: #if num is odd 3n+1
             result=3*result+1
             Lx.append(result)
         count+=1 
@@ -20,9 +22,16 @@ def collatz(num):
     return Lx,Ly
 
 if __name__=='__main__':
-    a=int(input("Enter Number you want to test:"))
-    print(collatz(a))
-    plt.scatter(collatz(a)[1],collatz(a)[0],s=2.5)
-    plt.grid()
-    plt.show()
+    st.title("Collatz Conjecture")
+    b=st.text_input("Enter Number you want to test:",value=1)
+    st.write(b)
+    a=int(b)
+    data1={
+        "X":collatz(a)[1],
+        "Y":collatz(a)[0]
+        } 
+    with st.container():
+     b=st.write(data1)
+     ch=pd.DataFrame(data1)
+     st.scatter_chart(ch)
     
