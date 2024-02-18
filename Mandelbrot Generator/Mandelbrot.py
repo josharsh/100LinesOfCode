dcce9269eec4385c4d1f28ed3395ed448b4d80ef
de-longhi <johandejongh665@gmail.com>
2024-02-18 13:09:56 +0200

    busy with documentation

--- Mandelbrot Generator/Mandelbrot.py
+++ Mandelbrot Generator/Mandelbrot.py
@@ -1,7 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def mandelbrot(c, max_iter):
+def mandelbrot(c : complex, max_iter : int) -> int:      
+    """
+    Returns the amount of recursive iterations used to guess whether the point is in the set.
+        Parameters
+        ----------
+        c : complex
+            The number to be calculated
+        max_iter : int
+            The number of iterations of recursion
+            
+        Raises
+        ------
+        TypeError
+            If number is not complex type or if max_iter is not integer.
+    """
+    if not isinstance(c, complex):
+        raise TypeError("Number must be complex type.")
+    if not isinstance(max_iter, int):
+        raise TypeError("Max iterations must be of type int.")
+    
     z = 0
     for n in range(max_iter):
         if abs(z) > 2:
@@ -9,7 +28,31 @@ def mandelbrot(c, max_iter):
         z = z*z + c
     return max_iter
 
-def mandelbrot_set(xmin,xmax,ymin,ymax,width,height,max_iter):
+def mandelbrot_set(xmin : float, xmax : float, ymin : float, ymax : float, width : int, height : int, max_iter : int) -> np.ndarray:
+    """
+    Returns a 2D np.array of size width * height with each element the point's corresponding amount of iterations of recursive calculations.
+        Parameters
+        ----------
+        xmin : float
+            The leftmost x-point on the image
+        xmax : float
+            The rightmost x-point on the image
+        ymin : float
+            The bottom y-point on the image
+        ymax : float
+            The top y-point on the image
+        width : int
+            The width of the image in pixels
+        height : int
+            The height of the image in pixels
+        max_inter : iter
+            The maximum amount of recursive iterations to be done before deciding whether a point is in the set
+
+        Raises
+        ------
+        TypeError
+            If number is not complex type or if max iter is not integer.
+    """
     x_space = np.linspace(xmin, xmax, width)
     y_space = np.linspace(ymin, ymax, height)
     return np.array([[mandelbrot(complex(x, y),max_iter) for x in x_space] for y in y_space])
@@ -23,6 +66,8 @@ def plot_mandelbrot(xmin,xmax,ymin,ymax,width,height,max_iter):
     plt.ylabel("Imaginary")
     plt.show()
 
+def _typecheck():
+    
 # Set the range and resolution for the plot
 xmin, xmax, ymin, ymax = -2.0, 1.0, -1.5, 1.5
 width, height = 1000, 1000
