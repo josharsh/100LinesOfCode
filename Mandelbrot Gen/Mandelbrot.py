2b4e9c69b40b17dbd406417858afcea200e642b1
Johan de Jongh <johandejongh665@gmail.com>
2024-03-12 11:36:40 +0200

    Johan de Jongh | Mandelbrot.py | Added documentation and more comments

--- Mandelbrot Gen/Mandelbrot.py
+++ Mandelbrot Gen/Mandelbrot.py
@@ -2,6 +2,24 @@ from PIL import Image
 import numpy as np
 
 def mandelbrot(c : complex, max_iter : int) -> int:      
+    """
+    Returns the amount of recursive iterations used to guess whether the point is in the set.
+        Parameters
+        ----------
+        c : complex
+            The number to be calculated
+        max_iter : int
+            The number of iterations of recursion
+            
+        Raises
+        ------
+        TypeError
+            If number is not complex type or if max_iter is not integer.
+    """
+    if not isinstance(c, complex):
+        raise TypeError("Number must be complex type.")
+    if not isinstance(max_iter, int):
+        raise TypeError("Max iterations must be of type int.")
     
     z = c
     for n in range(max_iter):
@@ -17,12 +35,50 @@ def _mandelbrot_set(xmin : float, xmax : float, ymin : float, ymax : float, widt
     return (np.array([[mandelbrot(complex(x, y),max_iter) for x in x_space] for y in y_space]))
 
 def plot_mandelbrot(xmin : float, xmax : float, ymin : float, ymax : float, width : int, height : int, max_iter : int):
+    """
+    Plots a mandelbrot set corresponding to the given parameters.
+        Parameters
+        ----------
+        xmin : float
+            The leftmost x-point on the image
+        xmax : float
+            The rightmost x-point on the image
+        ymin : float
+            The bottom y-point on the image
+        ymax : float
+            The top y-point on the image
+        width : int
+            The width of the image in pixels
+        height : int
+            The height of the image in pixels
+        max_inter : int
+            The maximum amount of recursive iterations to be done before deciding whether a point is in the set
+
+        Raises
+        ------
+        TypeError
+            If ranges are not floats or integers, or if sizes are not integers or if max_iter is not int
+    """
+    if not isinstance(xmin, (float, int)):
+        raise TypeError("All ranges must be of type float or int.")
+    if not isinstance(xmax, (float, int)):
+        raise TypeError("All ranges must be of type float or int.")
+    if not isinstance(ymin, (float, int)):
+        raise TypeError("All ranges must be of type float or int.")
+    if not isinstance(ymax, (float, int)):
+        raise TypeError("All ranges must be of type float or int.")
+    if not isinstance(width,  int):
+        raise TypeError("Sizes of image must be of type int.")
+    if not isinstance(height, int):
+        raise TypeError("Sizes of image must be of type int.")
+    if not isinstance(max_iter, int):
+        raise TypeError("Max iterations must be of size int.")
 
     set = _mandelbrot_set(xmin,xmax,ymin,ymax,width,height,max_iter)
     image = Image.new("RGB", (width, height))
     normalized_iterations = set / max_iter
 
-    # Here I iterate over each pixel to a cooler color scheme than without it.
+    # Here I iterate over each pixel to change the color scheme to look a bit cooler :0
     for y in range(height):
         for x in range(width):
             iter_count = normalized_iterations[y, x]
@@ -35,8 +91,8 @@ def plot_mandelbrot(xmin : float, xmax : float, ymin : float, ymax : float, widt
     image.show()
     
 if __name__ == "__main__":
-    # Set the range and resolution for the plot
     
+    # Set the range and resolution for the plot  
     xmin, xmax, ymin, ymax = -2.0, 1.0, -1.5, 1.5
     width, height = 1000, 1000
     max_iter = 80
