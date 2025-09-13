e02a808749bf5ac2c8358cfd14814b99a4e7548f
Garganis <igarganis@csd.auth.gr>
2025-09-14 00:45:44 +0300

    fixedUI
    fixed UI and added , for decimal operations

--- BasicCalculator/script.js
+++ BasicCalculator/script.js
@@ -7,28 +7,30 @@ const getkey = (key)=>{
                 if(key == '='){
                         output_win.innerHTML = "INVALID OPERATION";
                         expression = "";
+                        firstVal = true;
                 }
-
                 else{
                         expression = key;
-                        output_win.innerHTML += key;
+                        output_win.innerHTML = key; 
                         firstVal = false;
                 }
         }
         else{
                 if(key != '='){
                         expression += key;
-                        output_win.innerHTML += key;
+                        output_win.innerHTML = expression; 
                 }        
                 else{
                         try{
-
-                                output_win.innerHTML = eval(expression).toString();                
+                                let result = eval(expression).toString();
+                                output_win.innerHTML = result;
+                                expression = result; 
+                                firstVal = false; 
                         }
-
                         catch(err){
                                 output_win.innerHTML = "INVALID OPERATION";
                                 expression = "";
+                                firstVal = true;
                         }
                 }
         }
@@ -37,15 +39,48 @@ const getkey = (key)=>{
 const getClr = ()=>{
         output_win.innerHTML = "";
         expression = "";
+        firstVal = true;
 }
 
 const leftPar = ()=>{
         lp =  document.getElementById('btn-leftPar').value;
-        output_win.innerHTML += lp;
-        expression += lp;
+        if(firstVal){
+                expression = lp;
+                output_win.innerHTML = lp;
+                firstVal = false;
+        } else {
+                expression += lp;
+                output_win.innerHTML = expression;
+        }
 }
 const rightPar = ()=>{
         rp =  document.getElementById('btn-rightPar').value;
-        output_win.innerHTML += rp;
-        expression += rp;
+        if(firstVal){
+                expression = rp;
+                output_win.innerHTML = rp;
+                firstVal = false;
+        } else {
+                expression += rp;
+                output_win.innerHTML = expression;
+        }
+}
+
+const getDecimal = ()=>{
+        if(expression == "" || expression == undefined || firstVal){
+                expression = "0.";
+                output_win.innerHTML = "0.";
+                firstVal = false;
+        }
+        else{
+                
+                let lastOperatorIndex = Math.max(expression.lastIndexOf('+'), expression.lastIndexOf('-'), 
+                                               expression.lastIndexOf('*'), expression.lastIndexOf('/'),
+                                               expression.lastIndexOf('('));
+                let currentNumber = expression.substring(lastOperatorIndex + 1);
+                
+                if(!currentNumber.includes('.')){
+                        expression += ".";
+                        output_win.innerHTML = expression;
+                }
+        }
 }
