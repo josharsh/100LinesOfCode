a76bf5406cc9f557b02ac11fc7a88c09ef15f8bb
Diode-exe <DiodesCAER@protonmail.com>
2026-02-24 10:35:17 -0600

    Fixed f-strings within the project

--- automate_repo_creation/create.py
+++ automate_repo_creation/create.py
@@ -1,15 +1,18 @@
+"""Script to automate repository creation on github and local machine"""
+
+import json
 import os
 import sys
 import requests
 import yaml
-import json
 
-credentials = yaml.load(open("credentials.yaml"), Loader=yaml.FullLoader)
+credentials = yaml.load(open("credentials.yaml", encoding="utf-8"), Loader=yaml.FullLoader)
 
 def create():
+    """Creates a repository on github and local machine"""
     url = "https://api.github.com/user/repos"
     headers = {
-        "Authorization": "token {}".format(credentials["personal_acess_token"])
+        "Authorization": f"token {credentials['personal_acess_token']}"
     }
     parent_dir = credentials["path"]
     username = credentials["github_username"]
@@ -18,12 +21,12 @@ def create():
     try:
         os.makedirs(path)
     except Exception:
-        print("You have already created {} folder in path {}".format(repo_name, parent_dir))
+        print(f"You have already created {repo_name} folder in path {parent_dir}")
         exit(1)
     data = json.dumps({"name": repo_name, "description": "This is repo was created with script"})
 
     response = requests.post(url, data=data, headers=headers)
-    print("Succesfully created repository {}".format(repo_name))
+    print(f"Succesfully created repository {repo_name}")
 
 
 if __name__ == "__main__":
