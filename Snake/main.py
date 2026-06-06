d7f4d680687a0517c196fce7fdb9164731913bef
Azurrati <246398444+Azurrati@users.noreply.github.com>
2026-06-06 16:45:13 -0700

    Increased Simplicity and Readability.

--- Snake/main.py
+++ Snake/main.py
@@ -1,95 +1,95 @@
-import keyboard, time, random, os, threading
+import keyboard, time, random, os
 
-class Color: red, green, normal = '\033[31m', '\033[32m', '\033[0m'
+class Color:
+    red, green, normal = '\033[31m', '\033[32m', '\033[0m'
+
 
 class Snake:
-    def __init__(self, x_pos, y_pos):
-        self.x, self.y = x_pos, y_pos
+    def __init__(self):
+        self.x, self.y = random.randint(0, MAP_X_SIZE-1), random.randint(0, MAP_Y_SIZE-1)
         self.direction = None
         self.length = [(self.x, self.y)]
-    
-    def new_direction(self, key):
-        if key == 'a' and self.direction != 'R': self.direction = 'L'
-        elif key == 'd' and self.direction != 'L': self.direction = 'R'
-        elif key == 'w' and self.direction != 'D': self.direction = 'U'
-        elif key == 's' and self.direction != 'U': self.direction = 'D'
-    
-    def move(self):
-        if self.direction == 'L': self.length.append((self.x-1, self.y))
-        elif self.direction == 'R': self.length.append((self.x+1, self.y))
-        elif self.direction == 'U': self.length.append((self.x, self.y-1))
-        elif self.direction == 'D': self.length.append((self.x, self.y+1))
-        elif self.direction == None: self.length.append((self.x, self.y))
 
-        self.x, self.y = self.length[-1][0], self.length[-1][1]
+        self.key_map = { 'a': 'Left', 'd': 'Right', 'w': 'Up', 's': 'Down' }
+        self.direction_map = { 'Left': (-1, 0), 'Right': (1, 0), 'Up': (0, -1), 'Down': (0, 1), None: (0, 0) }
+        self.opposite = { 'Left': 'Right', 'Right': 'Left', 'Up': 'Down', 'Down': 'Up'}
+    
+
+    def new_direction(self, key):
+        if (key in self.key_map) and (self.direction != self.opposite[self.key_map[key]]):
+            self.direction = self.key_map[key]
+
+
+    def move(self):
+        dx, dy = self.direction_map[self.direction]
+        self.length.append((self.x + dx, self.y + dy))
+
+        self.x, self.y = self.length[-1]
         self.length.pop(0)
 
-        if self.length[-1] in self.length[:-1]: print('Game Over!'), exit()
-        if (self.x < 0) or (self.x > MAP_X_SIZE-1) or (self.y < 0) or (self.y > MAP_Y_SIZE-1): print('Game Over!'), exit()
+
+        if self.length[-1] in self.length[:-1]:
+            print('Game Over!')
+            exit()
+
+        if (self.x < 0) or (self.x > MAP_X_SIZE-1) or (self.y < 0) or (self.y > MAP_Y_SIZE-1):
+            print('Game Over!')
+            exit()
+
 
 class Apple:
-    def __init__(self, x_pos, y_pos):
-        self.x, self.y = x_pos, y_pos
+    def place(self):
+        while True:
+            self.x, self.y = random.randint(0, MAP_X_SIZE-1), random.randint(0, MAP_Y_SIZE-1)
 
-        while (self.x, self.y) in snake.length:
-            self.x = random.randint(0, MAP_X_SIZE-1)
-            self.y = random.randint(0, MAP_Y_SIZE-1)
-    
-    def eaten(self):
-        self.x = random.randint(0, MAP_X_SIZE-1)
-        self.y = random.randint(0, MAP_Y_SIZE-1)
+            if not (self.x, self.y) in snake.length: return
 
-        while (self.x, self.y) in snake.length:
-            self.x = random.randint(0, MAP_X_SIZE-1)
-            self.y = random.randint(0, MAP_Y_SIZE-1)
 
 class Renderer:
     def __init__(self):
         self.score = 0
-    
+
+
     def render(self):
-        if (snake.x == apple.x) and (snake.y == apple.y):
+        if (snake.x, snake.y) == (apple.x, apple.y):
             self.score += 1
-            apple.eaten()
-            snake.length.insert(0, snake.length[0])
+            apple.place()
+            snake.length.insert(0, 'SNAKE_GROWTH')
         snake.move()
 
-        os.system('clear' if os.name == 'posix' else 'cls')
+        os.system('clear' if os.name == 'posix' else 'cls')     # Clears for the next render
 
-        print('╔' + '═'*(MAP_X_SIZE*2) + '╗')
+        print('╔' + ('═' * MAP_X_SIZE * 2) + '╗')
 
         for y in range(MAP_Y_SIZE):
-            row = ''
-            row += '║'
+            row = '║'
 
             for x in range(MAP_X_SIZE):
-                if (x,y) in snake.length: row += Color.green + '[]' + Color.normal
-                elif (x == apple.x) and (y == apple.y): row += Color.red + '()' + Color.normal
-                else: row += ' .'
+                if (x, y) in snake.length:          row += f'{Color.green}[]{Color.normal}'
+                elif (x, y) == (apple.x, apple.y):  row += f'{Color.red}(){Color.normal}'
+                else:                               row += ' .'
 
             row += '║'
             print(row)
         
-        print('╚' + '═'*MAP_X_SIZE*2 + '╝')
+        print('╚' + ('═' * MAP_X_SIZE * 2) + '╝')
         print(f'Score: {self.score}')
 
-        if self.score >= ((MAP_X_SIZE * MAP_Y_SIZE) - 1): print('You Win!'), exit()
+        if self.score >= ((MAP_X_SIZE * MAP_Y_SIZE) - 1):
+            print('You Win!')
+            exit()
+
 
 MAP_X_SIZE = MAP_Y_SIZE = 10
 
-snake = Snake(random.randint(0, MAP_X_SIZE-1), random.randint(0, MAP_Y_SIZE-1))
-apple = Apple(random.randint(0, MAP_X_SIZE-1), random.randint(0, MAP_Y_SIZE-1))
+snake = Snake()
+apple = Apple()
 map = Renderer()
 
-def keyboard_handler():
-    while True:
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN: snake.new_direction(event.name)
-
-def map_renderer():
+if __name__ == '__main__':
+    apple.place()
+    keyboard.on_press(lambda key_input: snake.new_direction(key_input.name))    # This always checks for input, even if it's not in a loop
+    
     while True:
         map.render()
         time.sleep(0.15)
-
-threading.Thread(target=keyboard_handler, daemon=True).start()
-map_renderer()
